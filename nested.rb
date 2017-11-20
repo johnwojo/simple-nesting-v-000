63a68acdff209459822edd6bb90f7b173edbb6f1
John Wojciechowski <jwojo13@gmail.com>
2017-11-20 21:20:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 drlanguage = [:dennis_ritchie][:languages]
+		 dr_language = [:dennis_ritchie][:languages]
 end
 
 def adding_matz
